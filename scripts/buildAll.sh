2895163d2fdff73cffbc992dde5dbba698816897
MarvNC <rotrobmin@gmail.com>
2023-12-23 01:02:19 -0800

    Add script to download all from inputs.txt

---
+++ scripts/buildAll.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Read input from inputs.txt
+input_file="inputs.txt"
+IFS=',' read -r -a langs < <(sed -n '1p' "$input_file")
+date_val=$(sed -n '2p' "$input_file")
+
+# Format date for file naming
+dateFile=$(date -d "$date_val" +"%Y.%m.%d")
+
+# Output array and dateFile
+echo "langs array: ${langs[@]}"
+echo "date_val variable: $date_val"
+echo "dateFile variable: $dateFile"
+
+# For each lang, run downloadDump.sh with <lang> <dateFile>
+for lang in "${langs[@]}"
+do
+  echo "Running downloadDump.sh with $lang and $dateFile"
+  ./scripts/downloadDump.sh "$lang" "$dateFile"
+  # Run node src/convertWikipedia.js with <lang> <date_val>
+  echo "Running convertWikipedia.js with $lang and $date_val"
+  node src/convertWikipedia.js "$lang" "$date_val"
+done

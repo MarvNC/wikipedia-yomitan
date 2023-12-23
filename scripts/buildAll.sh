2f088fa76c6490b475a3d8cadbf4b42c4238b786
MarvNC <rotrobmin@gmail.com>
2023-12-23 01:27:56 -0800

    Fix buildAll

--- scripts/buildAll.sh
+++ scripts/buildAll.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Read input from inputs.txt
-input_file="inputs.txt"
-IFS=',' read -r -a langs < <(sed -n '1p' "$input_file")
-date_val=$(sed -n '2p' "$input_file")
+langs=("ja" "zh")
+date_val="2022-12-01"
 
 # Format date for file naming
 dateFile=$(date -d "$date_val" +"%Y.%m.%d")
@@ -14,15 +12,13 @@ echo "date_val variable: $date_val"
 echo "dateFile variable: $dateFile"
 
 # For each lang, run downloadDump.sh with <lang> <dateFile>
-for lang in "${langs[@]}"
-do
+for lang in "${langs[@]}"; do
   echo "Running downloadDump.sh with $lang and $dateFile"
   ./scripts/downloadDump.sh "$lang" "$dateFile"
 done
 
 # Run node src/convertWikipedia.js with <lang> <date_val> for each lang
-for lang in "${langs[@]}"
-do
+for lang in "${langs[@]}"; do
   echo "Running convertWikipedia.js with $lang and $date_val"
   node src/convertWikipedia.js "$lang" "$date_val"
 done

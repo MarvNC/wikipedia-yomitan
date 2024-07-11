b08cbce9d30bce771e86fd1d399950a2f9a42244
MarvNC <rotrobmin@gmail.com>
2024-07-11 15:04:05 -0700

    Convert getVersion.js to TypeScript

--- src/util/getVersion.ts
+++ src/util/getVersion.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-export function getVersion() {
+export function getVersion(): string {
   const packageJsonPath = path.join(process.cwd(), 'package.json');
   const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+  if (!packageJson.version) {
+    throw new Error('Could not get version from package.json');
+  }
   return packageJson.version;
 }
